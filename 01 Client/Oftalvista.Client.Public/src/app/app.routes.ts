438a2f8db3baf9c6fce5d175de956d5208b396c0
Víctor Manuel Cuadrado Hidalgo <usitd05@sunedu.gob.pe>
2026-04-29 22:27:14 -0500

    chore : configuración del routes

--- 01 Client/Oftalvista.Client.Public/src/app/app.routes.ts
+++ 01 Client/Oftalvista.Client.Public/src/app/app.routes.ts
@@ -2,6 +2,36 @@ import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth.guard';
 import { adminGuard } from './core/guards/admin.guard';
 export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./modules/auth/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'dashboard/admin',
+    canActivate: [authGuard, adminGuard],
+    loadComponent: () =>
+      import('./modules/dashboard/admin/dashboard-admin.component').then(
+        (m) => m.DashboardAdminComponent,
+      ),
+  },
+  {
+    path: 'dashboard/paciente',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./modules/dashboard/paciente/dashboard-paciente.component').then(
+        (m) => m.DashboardPacienteComponent,
+      ),
+  },
+  {
+    path: 'operaciones/recordatorios',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./modules/recordatorio-cita/list/recordatorio-cita-list.component').then(
+        (m) => m.RecordatorioCitaListComponent,
+      ),
+  },
   {
     path: 'mantenimiento/usuarios',
     canActivate: [authGuard, adminGuard],
@@ -15,5 +45,6 @@ export const routes: Routes = [
       import('./modules/agenda-medica/list/agenda-medica-list.component').then(
         (m) => m.AgendaMedicaListComponent,
       ),
-  }
+  },
+  { path: '**', redirectTo: 'login' },
 ];
