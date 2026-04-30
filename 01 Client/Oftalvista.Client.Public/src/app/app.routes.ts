94e1c74f5049bb8d321d4e18ca8ff0590c66e995
Víctor Manuel Cuadrado Hidalgo <usitd05@sunedu.gob.pe>
2026-04-29 22:42:32 -0500

    chore : configuración del routes

--- 01 Client/Oftalvista.Client.Public/src/app/app.routes.ts
+++ 01 Client/Oftalvista.Client.Public/src/app/app.routes.ts
@@ -2,6 +2,50 @@ import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth.guard';
 import { adminGuard } from './core/guards/admin.guard';
 export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./modules/auth/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'dashboard/admin',
+    canActivate: [authGuard, adminGuard],
+    loadComponent: () =>
+      import('./modules/dashboard/admin/dashboard-admin.component').then(
+        (m) => m.DashboardAdminComponent,
+      ),
+  },
+  {
+    path: 'dashboard/paciente',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./modules/dashboard/paciente/dashboard-paciente.component').then(
+        (m) => m.DashboardPacienteComponent,
+      ),
+  },
+  {
+    path: 'operaciones/recordatorios',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./modules/recordatorio-cita/list/recordatorio-cita-list.component').then(
+        (m) => m.RecordatorioCitaListComponent,
+      ),
+  },
+  {
+    path: 'mantenimiento/usuarios',
+    canActivate: [authGuard, adminGuard],
+    loadComponent: () =>
+      import('./modules/usuario/list/usuario-list.component').then((m) => m.UsuarioListComponent),
+  },
+  {
+    path: 'mantenimiento/agenda-medica',
+    canActivate: [authGuard, adminGuard],
+    loadComponent: () =>
+      import('./modules/agenda-medica/list/agenda-medica-list.component').then(
+        (m) => m.AgendaMedicaListComponent,
+      ),
+  },
   {
     path: 'mantenimiento/medicos',
     canActivate: [authGuard, adminGuard],
@@ -16,4 +60,5 @@ export const routes: Routes = [
         (m) => m.HistorialCitaListComponent,
       ),
   },
+  { path: '**', redirectTo: 'login' },
 ];
