65cc5f570ccbd26c46b404cbe7d0fa01075e1733
Kelly Vargas <u202322081@upc.edu.pe>
2026-04-29 22:41:00 -0500

    Gestión de médico e historial

--- 01 Client/Oftalvista.Client.Public/src/app/app.routes.ts
+++ 01 Client/Oftalvista.Client.Public/src/app/app.routes.ts
@@ -2,49 +2,18 @@ import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth.guard';
 import { adminGuard } from './core/guards/admin.guard';
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
-    path: 'login',
-    loadComponent: () =>
-      import('./modules/auth/login/login.component').then((m) => m.LoginComponent),
-  },
-  {
-    path: 'dashboard/admin',
+    path: 'mantenimiento/medicos',
     canActivate: [authGuard, adminGuard],
     loadComponent: () =>
-      import('./modules/dashboard/admin/dashboard-admin.component').then(
-        (m) => m.DashboardAdminComponent,
-      ),
+      import('./modules/medico/list/medico-list.component').then((m) => m.MedicoListComponent),
   },
   {
-    path: 'dashboard/paciente',
+    path: 'operaciones/historial',
     canActivate: [authGuard],
     loadComponent: () =>
-      import('./modules/dashboard/paciente/dashboard-paciente.component').then(
-        (m) => m.DashboardPacienteComponent,
+      import('./modules/historial-cita/list/historial-cita-list.component').then(
+        (m) => m.HistorialCitaListComponent,
       ),
   },
-  {
-    path: 'operaciones/recordatorios',
-    canActivate: [authGuard],
-    loadComponent: () =>
-      import('./modules/recordatorio-cita/list/recordatorio-cita-list.component').then(
-        (m) => m.RecordatorioCitaListComponent,
-      ),
-  },
-  {
-    path: 'mantenimiento/usuarios',
-    canActivate: [authGuard, adminGuard],
-    loadComponent: () =>
-      import('./modules/usuario/list/usuario-list.component').then((m) => m.UsuarioListComponent),
-  },
-  {
-    path: 'mantenimiento/agenda-medica',
-    canActivate: [authGuard, adminGuard],
-    loadComponent: () =>
-      import('./modules/agenda-medica/list/agenda-medica-list.component').then(
-        (m) => m.AgendaMedicaListComponent,
-      ),
-  },
-  { path: '**', redirectTo: 'login' },
 ];
